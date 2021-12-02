d8d301436961d0d9f220bb1b9f0449e8858e96eb
Rodriguez-Jp <juanrdrz16@gmail.com>
2021-12-02 15:24:44 -0500

    First commit, adding some layout.

---
+++ app.js
@@ -0,0 +1,28 @@
+const apiUrl = 'https://opentdb.com/api.php?amount=1';
+const random = Math.floor(Math.random() * 4);
+
+fetch(apiUrl).then((response) => response.json().then((qs) => {
+    
+    let answers = [
+        qs.results[0].question
+    ]
+
+    alert(answers);
+}))
+
+
+// Shows the data
+
+
+const question_data = document.querySelector('.question');
+const answers = document.querySelectorAll('.res');
+
+fetch(apiUrl).then((response) => response.json().then((question) => {
+
+    console.log(question.results[0]);
+
+    question_data.innerHTML = question.results[0].question;
+
+    answers.innerText = question.results[0].question.incorrect
+
+}))
