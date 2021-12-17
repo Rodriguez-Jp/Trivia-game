dc904587b52099c4864ef6817258687c023a5647
Rodriguez-Jp <juanrdrz16@gmail.com>
2021-12-17 10:13:52 -0500

    Make the leadeboard functionality

--- leaderboard.js
+++ leaderboard.js
@@ -1,4 +1,50 @@
-const formattedScoreList = JSON.parse(localStorage.getItem('scores'));
-console.log(formattedScoreList);
 
-console.log(formattedScoreList.length);
+const scoreList = JSON.parse(localStorage.getItem('HighScores'));
+
+//In case there are no scores yet
+if (scoreList === null){
+    setTimeout( () => {
+        alert('There are no highscores yet, play to list yours!');
+    }, 1200)
+    
+} 
+
+// Sort the list based on the score
+function compare(a,b) { return b.score - a.score };
+scoreList.sort(compare);
+
+
+//Append the scores to the leaderboard
+const table = document.querySelector('.table');
+
+
+for (let i = 0; i < scoreList.length; i++){
+    const tr  = document.createElement('tr');
+    let thRank = document.createElement('th');
+    let thName = document.createElement('th');
+    let thScore = document.createElement('th');
+    thRank.innerHTML = `${i + 1}`;
+    thName.innerHTML = scoreList[i].name;
+    thScore.innerHTML = scoreList[i].score;
+    tr.appendChild(thRank);
+    tr.appendChild(thName);
+    tr.appendChild(thScore);
+    table.appendChild(tr);
+    console.log(tr, thName)
+}
+
+
+
+
+
+//Deletes all the localStorage data and clear the leaderboard
+const clearBtn = document.querySelector('.clear-btn');
+
+clearBtn.addEventListener('click', () => {
+    localStorage.clear();
+    window.location.reload(true);
+})
+
+
+
+
